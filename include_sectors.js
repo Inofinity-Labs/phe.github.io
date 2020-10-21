e9701e5dd2c4d556934a5006656a126e3de91934
pubudu-wijekoon <37551112+pubudu-wijekoon@users.noreply.github.com>
2020-10-21 19:15:48 +0530

    add details wind / solar

---
+++ include_sectors.js
@@ -0,0 +1,243 @@
+
+//Hydro Power -----------------------------------------------------------------------------
+    const hydro_xhr = new XMLHttpRequest();
+    const hydro_container = document.getElementById('include-content');
+    hydro_xhr.onload = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        hydro_container.innerHTML = hydro_xhr.responseText;
+        }else{
+            console.warn('Did not reserve');
+        }
+    };
+
+    hydro_xhr.open("GET", "includes/hydro.html");
+    hydro_xhr.send();
+
+    function hydro_loadhome(){
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/hydro.html", false);
+        xhttp.send();
+        document.getElementById("include-content").innerHTML = xhttp.responseText; 
+    }
+
+    function load1proj_dev() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/hydro/proj_dev.html", false);
+        xhttp.send();
+        document.getElementById("include-content").innerHTML = xhttp.responseText; 
+    }
+
+    function load1feas_stu() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/hydro/feas_stu.html", false);
+        xhttp.send();
+        document.getElementById("include-content").innerHTML = xhttp.responseText; 
+    }
+
+    function load1de_eng() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/hydro/de_eng.html", false);
+        xhttp.send();
+        document.getElementById("include-content").innerHTML = xhttp.responseText; 
+    }
+
+    function load1epc() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/hydro/epc.html", false);
+        xhttp.send();
+        document.getElementById("include-content").innerHTML = xhttp.responseText; 
+    }
+    
+    function load1pm() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/hydro/pm.html", false);
+        xhttp.send();
+        document.getElementById("include-content").innerHTML = xhttp.responseText; 
+    }
+
+    function load1om() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/hydro/om.html", false);
+        xhttp.send();
+        document.getElementById("include-content").innerHTML = xhttp.responseText; 
+    }
+
+    function load1sup_e() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/hydro/sup_e.html", false);
+        xhttp.send();
+        document.getElementById("include-content").innerHTML = xhttp.responseText; 
+    }
+
+    function load1supGRP() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/hydro/supGRP.html", false);
+        xhttp.send();
+        document.getElementById("include-content").innerHTML = xhttp.responseText; 
+    }
+    
+//Wind Power -----------------------------------------------------------------------------
+    const wind_xhr = new XMLHttpRequest();
+    const wind_container = document.getElementById('wind');
+    wind_xhr.onload = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        wind_container.innerHTML = wind_xhr.responseText;
+        }else{
+            console.warn('Did not reserve');
+        }
+    };
+
+    wind_xhr.open("GET", "includes/wind.html");
+    wind_xhr.send();
+
+    function wind_loadhome(){
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/wind.html", false);
+        xhttp.send();
+        document.getElementById("wind").innerHTML = xhttp.responseText; 
+    }
+
+
+    function load2_feasibility() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/wind/feasibility.html", false);
+        xhttp.send();
+        document.getElementById("wind").innerHTML = xhttp.responseText; 
+    }
+
+    function load2_simulation() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/wind/simulation.html", false);
+        xhttp.send();
+        document.getElementById("wind").innerHTML = xhttp.responseText; 
+    }
+
+    function load2_micro() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/wind/micro.html", false);
+        xhttp.send();
+        document.getElementById("wind").innerHTML = xhttp.responseText; 
+    }
+    
+    function load2_detaile() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/wind/detaile.html", false);
+        xhttp.send();
+        document.getElementById("wind").innerHTML = xhttp.responseText; 
+    }
+
+    function load2_projectm() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/wind/projectm.html", false);
+        xhttp.send();
+        document.getElementById("wind").innerHTML = xhttp.responseText; 
+    }
+
+//Solar Power -----------------------------------------------------------------------------    
+    const solar_xhr = new XMLHttpRequest();
+    const solar_container = document.getElementById('solar');
+    solar_xhr.onload = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        solar_container.innerHTML = solar_xhr.responseText;
+        }else{
+            console.warn('Did not reserve');
+        }
+    };
+
+    solar_xhr.open("GET", "includes/solar.html");
+    solar_xhr.send();
+
+    function solar_loadhome(){
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/solar.html", false);
+        xhttp.send();
+        document.getElementById("solar").innerHTML = xhttp.responseText; 
+    }
+
+
+    function solar_feasibility() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/solar/solar_feasibility.html", false);
+        xhttp.send();
+        document.getElementById("solar").innerHTML = xhttp.responseText; 
+    }
+
+    function solar_detailed() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/solar/solar_detailed.html", false);
+        xhttp.send();
+        document.getElementById("solar").innerHTML = xhttp.responseText; 
+    }
+
+    function solar_simulation() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/solar/solar_simulation.html", false);
+        xhttp.send();
+        document.getElementById("solar").innerHTML = xhttp.responseText; 
+    }
+
+    function solar_projectm() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/solar/solar_projectm.html", false);
+        xhttp.send();
+        document.getElementById("solar").innerHTML = xhttp.responseText; 
+    }
+
+    function solar_installation() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/solar/solar_installation.html", false);
+        xhttp.send();
+        document.getElementById("solar").innerHTML = xhttp.responseText; 
+    }
+
+    function solar_epc() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/solar/solar_epc.html", false);
+        xhttp.send();
+        document.getElementById("solar").innerHTML = xhttp.responseText; 
+    }
+
+//Bio  -----------------------------------------------------------------------------    
+    const bio_xhr = new XMLHttpRequest();
+    const bio_container = document.getElementById('bio');
+    bio_xhr.onload = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        bio_container.innerHTML = bio_xhr.responseText;
+        }else{
+            console.warn('Did not reserve');
+        }
+    };
+
+    bio_xhr.open("GET", "includes/bio.html");
+    bio_xhr.send();
+
+    function bio_loadhome(){
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/bio.html", false);
+        xhttp.send();
+        document.getElementById("bio").innerHTML = xhttp.responseText; 
+    }
+
+
+    function bio_feasibility() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/bio/feasibility.html", false);
+        xhttp.send();
+        document.getElementById("bio").innerHTML = xhttp.responseText; 
+    }
+
+    function bio_detailed() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/bio/detailede.html", false);
+        xhttp.send();
+        document.getElementById("bio").innerHTML = xhttp.responseText; 
+    }
+
+    function bio_project() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "includes/bio/projectm.html", false);
+        xhttp.send();
+        document.getElementById("bio").innerHTML = xhttp.responseText; 
+    }
+
+    
