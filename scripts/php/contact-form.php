0ee60acf3a1c139429110a01c9460b110a0cee30
Parakum Dissanayake <parakumbbb@gmail.com>
2021-01-13 13:11:04 +0530

    contact

--- scripts/php/contact-form.php
+++ scripts/php/contact-form.php
@@ -6,7 +6,7 @@ if (file_exists("../../../../../wp-load.php")) {
     $isWP = true;
 }
 
-$emailTo       = 'info@pacific.lk';
+$emailTo       = 'parakumbbb@gmail.com';
 $sender_email = 'contacts@framework-y.com';
 $subject = 'You received a new message';
 
